ac9ef8e6edaaf00d4030f61ec2c02fc07f8827a9
Devanshpatel350 <devanshpatel350@gmail.com>
2026-05-26 13:09:16 -0400

    Added Linked List operations and loop detection in lec16.java

---
+++ lec16.java
@@ -0,0 +1,109 @@
+// // class LinkedList{
+// //     void print(){
+
+// //     }
+// //     public static void main(String[] args) {
+// //         LinkedList list = new LinkedList();
+// //         list.insert(1);
+// //         list.insert(2);
+// //         list.insert(3);
+// //         list.insert(4);
+// //         list.insert(5);
+// //         list.head.next.next.next.next = list.head.next;
+// //         if(list.hasLoop()){
+// //             System.out.println("Loop Detected");
+// //         }
+// //         else{
+// //             System.out.println("Loop not Detected");
+// //         }
+// //         //list.nthNode(2);
+// //     }
+// // }
+
+
+// // class LinkedList{
+// //     void middlenode(){
+
+// //     }
+// //     System.out.println(slow.)
+
+// // }
+
+
+
+// // class LinkedList{
+// //     viod nthNode(int n){
+// //         Node first = head;
+// //         Node second = head;
+// //         for(int i=0; i<n; i++){
+// //             if(first == null){
+// //                 System.out.println("list is empty shrt");
+// //             }
+// //             first = first.next;
+// //         }
+// //         while(first != null){
+// //             first = first next;
+// //             second = second.next;
+// //         } 
+// //     }   System.out.println(second.data);
+
+// // }
+// // Node merge(Node head1, Node head2){
+
+// // }
+// // void println(){
+// //     Node temp = head;
+// //      while(temp != null){
+// //         System.out.println(team.data + "");
+// //         team = team.next;
+// //      }
+// //      System.out.println("null");
+// // }
+// // public static void main(static[] args){
+// //     LinkedList list1 = new LinkedList();
+// //     list1.insert(10);
+// //      list1.insert(10);
+// //       list1.insert(10);
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package LinkedList;
+
+// class LinkedList{
+//     Node head;
+//     void insert(int data ){
+//         Node newNode= new Node(data);
+//         if(head == null){
+//             head=newNode;
+//             return;
+//         }
+//         Node temp =head;
+//         while(temp.next !=null){
+//             temp= temp.next;
+//         }
+//         temp.next= newNode;
+//     }
+
+//     void print(){
+//         Node.temp=head;
+//     }
+// }
+// public class new {
+
+
+    
+//     public static void main (String [] args){
+
+//     }
+// }
