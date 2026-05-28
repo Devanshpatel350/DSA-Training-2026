efd348f9e83d4fb8467fbba7ad23e45f8888a1e0
Devanshpatel350 <devanshpatel350@gmail.com>
2026-05-28 14:00:00 -0400

    Add butterfly pattern and LinkedList implementation in lec18 and linkedlist

---
+++ lec18.java
@@ -0,0 +1,20 @@
+//butterfly
+
+public class lec18 {
+
+    public static void butterfly(int n) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+                
+            }
+            for(int j)
+
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=5;
+        butterfly(n);
+    }
+}
