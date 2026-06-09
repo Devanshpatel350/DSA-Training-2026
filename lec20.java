bcd342a2037b012f95ae77070e9574424585ba11
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-09 12:30:02 -0400

    Implemented binary search and practiced array reverse operations in Java

--- lec20.java
+++ lec20.java
@@ -1,43 +1,43 @@
-//binarySearch
+// //binarySearch
 
 
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class lec20 {
+// public class lec20 {
 
-    public static int  binarySearch(int num[],int key) {
+//     public static int  binarySearch(int num[],int key) {
 
-        int start=0 , end=num.length-1;
-        int mid=0;
-        while(start<=end){
-             mid = (start+end)/2;
+//         int start=0 , end=num.length-1;
+//         int mid=0;
+//         while(start<=end){
+//              mid = (start+end)/2;
 
-            if(num[mid]==key){
-                // System.out.println("found in "+mid);
-                return mid;
-            }else if(num[mid]>key){
-                end=mid-1;
-            }else {
-                start=mid+1;
-            }
+//             if(num[mid]==key){
+//                 // System.out.println("found in "+mid);
+//                 return mid;
+//             }else if(num[mid]>key){
+//                 end=mid-1;
+//             }else {
+//                 start=mid+1;
+//             }
            
 
 
-        }
-         return -1;
-    }
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("SEARCH NUM B/W 1 - 58 ");
+//         }
+//          return -1;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         System.out.println("SEARCH NUM B/W 1 - 58 ");
 
-        int num[]={1,2,3,4,5,6,7,8,9,10,11,12,13,24,36,47,58};
-        int key=sc.nextInt();
-        int ans=binarySearch(num, key);
-        System.out.println("KEY FIND IN -> "+ans);
-        // System.out.println(binarySearch(num, key));
-    }
-}
+//         int num[]={1,2,3,4,5,6,7,8,9,10,11,12,13,24,36,47,58};
+//         int key=sc.nextInt();
+//         int ans=binarySearch(num, key);
+//         System.out.println("KEY FIND IN -> "+ans);
+//         // System.out.println(binarySearch(num, key));
+//     }
+// }
 
 //reverseArray
 public class reverseArray {
