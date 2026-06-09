0b9f6219ac4306901958912d04d23e27d8fb5e30
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-09 12:30:45 -0400

    Implemented binary search and practiced array reverse operations in Java

--- lec20.java
+++ lec20.java
@@ -39,20 +39,20 @@
 //     }
 // }
 
-//reverseArray
-public class reverseArray {
-    public static int [] reverse(int num[]) {
-        int len= num.length-1;
-        // int j=0;
-        for(int i=len,j=0;i>=0,j<len;i++,j++){
-            num[j]=num[i];
+// //reverseArray
+// public class reverseArray {
+//     public static int [] reverse(int num[]) {
+//         int len= num.length-1;
+//         // int j=0;
+//         for(int i=len,j=0;i>=0,j<len;i++,j++){
+//             num[j]=num[i];
             
-        }
-        return num[];
-    }
-    public static void main(String[] args) {
-        int num[]={1,2,4,6,8};
-        // int rever[]={};
-        reverse(num);
-    }
-}
+//         }
+//         return num[];
+//     }
+//     public static void main(String[] args) {
+//         int num[]={1,2,4,6,8};
+//         // int rever[]={};
+//         reverse(num);
+//     }
+// }
