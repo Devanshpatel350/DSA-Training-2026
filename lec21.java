96ed91d050cdfd2aa23f3f457c2e7625f871bde5
Devanshpatel350 <devanshpatel350@gmail.com>
2026-06-18 13:20:26 -0400

    Add Prefix Sum approach for Maximum Subarray Sum in lec21.java

--- lec21.java
+++ lec21.java
@@ -1,42 +1,42 @@
-//MaxSumArrSumPrefix
+// //MaxSumArrSumPrefix
 
-public class lec21 {
-    public static void maxSum(int num[] ) {
+// public class lec21 {
+//     public static void maxSum(int num[] ) {
         
-        int len= num.length;
-        int num2[]=new int[len];
-        int maxSum=0;
-        num2[0]=num[0];
-        for(int i=1;i<len;i++){
+//         int len= num.length;
+//         int num2[]=new int[len];
+//         int maxSum=0;
+//         num2[0]=num[0];
+//         for(int i=1;i<len;i++){
             
-            num2[i]=num[i]+num2[i-1];
-        }
+//             num2[i]=num[i]+num2[i-1];
+//         }
 
-        for(int i=0;i<len;i++){
-            int currSum=0;
-            for(int j=i+1;j<len;j++){
-                currSum=i==0?num2[j]:num2[j]-num2[i-1];
+//         for(int i=0;i<len;i++){
+//             int currSum=0;
+//             for(int j=i+1;j<len;j++){
+//                 currSum=i==0?num2[j]:num2[j]-num2[i-1];
 
-                if(currSum>maxSum){
-                    maxSum=currSum;
-                }
+//                 if(currSum>maxSum){
+//                     maxSum=currSum;
+//                 }
 
-            }
-        }
-        System.out.println(maxSum);
-        // return num2;
-        //  for(int i=0;i<len;i++){
+//             }
+//         }
+//         System.out.println(maxSum);
+//         // return num2;
+//         //  for(int i=0;i<len;i++){
            
-        //     System.out.println( num2[i]);
-        // }
+//         //     System.out.println( num2[i]);
+//         // }
        
-    }
-    public static void main(String[] args) {
+//     }
+//     public static void main(String[] args) {
 
-        int num[]={1,-2,4,-3,8,-5};
-        maxSum(num);
+//         int num[]={1,-2,4,-3,8,-5};
+//         maxSum(num);
        
 
         
-    }
-}
+//     }
+// }
